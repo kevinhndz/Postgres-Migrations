2f6a96dd6b039f696287b6ee06211e137323115d
Kevin <kh925063@gmail.com>
2026-09-06 13:13:53 -0600

    Agrega migracion 08: Insercion de Tabla Actividades & Horarios

---
+++ supabase/migrations/20260906191225_08_insertar_actividades.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO Actividades (TipoActividad) VALUES
+('Yoga'), ('Pilates'), ('Crossfit'), ('Boxeo'), ('Zumba'), ('Spinning'), ('Natacion'),
+('Pesas'), ('Calistenia'), ('Kickboxing'), ('Funcional'), ('MMA'), ('Aerobicos'),
+('Taekwondo'), ('Judo'), ('Fisicoculturismo'), ('BodyPump'), ('TRX'), ('Meditacion'), ('Baile');
+
+INSERT INTO Horarios_Actividades (Horario) VALUES
+('06:00 AM'), ('07:00 AM'), ('08:00 AM'), ('09:00 AM'), ('10:00 AM'), ('11:00 AM'), ('12:00 PM'),
+('01:00 PM'), ('02:00 PM'), ('03:00 PM'), ('04:00 PM'), ('05:00 PM'), ('06:00 PM'), ('07:00 PM'),
+('08:00 PM'), ('09:00 PM'), ('05:00 AM'), ('05:30 AM'), ('10:00 PM'), ('11:00 PM');
